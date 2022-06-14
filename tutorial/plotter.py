fe43b9c2fed70c12cd6b7d944814e28921290b1d
John <john.rotter@ufl.edu>
2022-06-14 12:49:33 -0500

    Updated plotter w gausppar

--- tutorial/plotter.py
+++ tutorial/plotter.py
@@ -1,12 +1,21 @@
 ############## IMPORTS ##############
 
-# import ROOT
-# import math
+import ROOT
+import math
 
 ############## FUNCTIONS ##############
-# We want function that const*e^(-(x-mu)*(x-mu) / ((2*sigma)*(2*sigma))) + a + b*x + c*x*x
+# We want function that const*e^(-(x-mu)*(x-mu) / ((2*sigma)*(2*sigma))) + a + b*x + c*x*x DONE!
 
-
+def gausppar(xs, params):
+   """
+      param[0] -> const
+      param[1] -> mu
+      param[2] -> sigma
+      param[3] -> a
+      param[4] -> b
+      param[5] -> c
+   """
+   return params[0] * math.exp((xs[0] - params[1])*(xs[0] - params[1]) / ((2*params[2])*(2*params[2]))) + params[3] + params[4]*xs[0] + params[5]*xs[0]*xs[0];
 
 ############## MAIN SCRIPT CALL ##############
 
